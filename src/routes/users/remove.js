63e74ddeffe5e817bbb199b0557e4a083db0dbcb
Leonan Luppi <leonan.luppi@gmail.com>
2018-01-12 11:59:59 -0200

    init

---
+++ src/routes/users/remove.js
@@ -0,0 +1,18 @@
+const User = require('./../../schema/user')
+
+module.exports = (req, res) => {
+    User
+        .findByIdAndRemove(req.params.id)
+        .then(() => {
+            return res.status(204).end()
+            // return res.status(200).json({
+            //     status: true
+            // })
+        })
+        .catch((err) => {
+            return res.status(500).json({
+                status: false,
+                err
+            })
+        })
+}
